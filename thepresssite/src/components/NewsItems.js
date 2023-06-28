31f1d8ef1bed630dbdd68f4e78a05f1ab9abc707
Niiischal <nischal.khatiwada745@gmail.com>
2023-06-28 13:11:23 +0545

    changing the structure of news items.

--- thepresssite/src/components/NewsItems.js
+++ thepresssite/src/components/NewsItems.js
@@ -4,18 +4,17 @@ const NewsItems = (props) => {
     // Destructuring the elemets to access from the api
     const {title, description, publishedTime, author, image, newsUrl}=props
   return (
-    <div className='my-3'>
-<div className="max-w-xs bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-        <img className="rounded-t-lg" src={image} alt="" />
-    <div className="p-5">
-            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{title}</h5>
-        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{description}</p>
-        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Time: {publishedTime}</p>
-        <h5 className="mb-2 text-xl tracking-tight text-gray-900 dark:text-white">Author: {author}</h5>
-        <a href={newsUrl} target="_blank" className="btn btn-sm btn-dark">Read More</a>
-    </div>
-</div>
-    </div>
+      <div className='my-3'>
+        <div className="card">
+          <img src={!image?"https://i.ytimg.com/vi/Jvpt-3VaIH0/maxresdefault.jpg":image} className="card-img-top" alt="..."/>
+            <div className="card-body">
+              <h3 className="card-title fs-2">{title}</h3>
+              <p className="card-text">{description}</p>
+              <p className="card-text"><small className="text-body-secondary">By: {!author?"Unknown" :author} on {new Date(publishedTime).toGMTString()}</small></p>
+              <a href={newsUrl} className="btn btn-sm btn-dark">Read More</a>
+            </div>
+       </div>
+      </div>
 
 
   )
