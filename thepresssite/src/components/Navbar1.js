1cdd9109c57b72472b40450a88fba2ea73e9fdc4
Niiischal <nischal.khatiwada745@gmail.com>
2023-06-29 15:00:19 +0545

    testing another navbar in navbar1 file

--- thepresssite/src/components/Navbar1.js
+++ thepresssite/src/components/Navbar1.js
@@ -1,40 +1,72 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 import '../style.css'
 
 const Navbar1 = () => {
+    const navNames={
+        padding:'10px',
+        textAlign:'center',
+        fontSize:'1rem',
+    }
+    
+    const navNames1={
+        padding:'15px',
+        textAlign:'center',
+        fontSize:'1rem',
+    }
   return (
     <div>
-      <nav class="navbar fixed-top">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="/" style={{color:'#fff', fontWeight:'700', letterSpacing:'2px'}}>ThePressSite</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+      <nav className="navbar fixed-top">
+  <div className="container-fluid" >
+    <a className="navbar-brand" href="#" style={{color:'#fff', fontWeight:'700', letterSpacing:'2.5px'}}>ThePressSite</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar" aria-label="Toggle navigation" style={{backgroundColor:'#fff'}}>
+      <span className="navbar-toggler-icon" style={{color:"white"}}></span>
     </button>
-    <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
-      <div class="offcanvas-header">
-        <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Offcanvas</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close" style={{color:'black'}}></button>
+    <div className="offcanvas offcanvas-end text-bg-dark" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
+      <div className="offcanvas-header">
+        <h5 className="offcanvas-title" id="offcanvasDarkNavbarLabel" style={{color:'#fff', fontWeight:'700', letterSpacing:'2.5px', fontSize:'1.3rem'}}>Navigation</h5>
       </div>
-      <div class="offcanvas-body">
-        <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
+      <div className="offcanvas-body">
+        <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
+          <li className="nav-item" style={navNames}>
+          <NavLink to="/" className="links" style={({ isActive }) => ({
+    color: isActive ? 'red' : '#fff',
+  })}>Home</NavLink>
           </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
+          <li className="nav-item" style={navNames1}>
+          <NavLink to="/general" className="links" style={({ isActive }) => ({
+    color: isActive ? 'red' : '#fff',
+  })}>General</NavLink>
           </li>
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              Dropdown
-            </a>
-            <ul class="dropdown-menu">
-              <li><a class="dropdown-item" href="#">Action</a></li>
-              <li><a class="dropdown-item" href="#">Another action</a></li>
-              <li>
-                <hr class="dropdown-divider"/>
-              </li>
-              <li><a class="dropdown-item" href="#">Something else here</a></li>
-            </ul>
+          <li className="nav-item" style={navNames1}>
+          <NavLink to="/business" className="links" style={({ isActive }) => ({
+    color: isActive ? 'red' : '#fff',
+  })}>Business</NavLink>
+          </li>
+          <li className="nav-item" style={navNames1}>
+          <NavLink to="/entertainment" className="links" style={({ isActive }) => ({
+    color: isActive ? 'red' : '#fff',
+  })}>Entertainment</NavLink>
+          </li>
+          <li className="nav-item" style={navNames1}>
+          <NavLink to="/sports" className="links" style={({ isActive }) => ({
+    color: isActive ? 'red' : '#fff',
+  })}>Sports</NavLink>
+          </li>
+          <li className="nav-item" style={navNames1}>
+          <NavLink to="/health" className="links" style={({ isActive }) => ({
+    color: isActive ? 'red' : '#fff',
+  })}>Health</NavLink>
+          </li>
+          <li className="nav-item" style={navNames1}>
+          <NavLink to="/science" className="links" style={({ isActive }) => ({
+    color: isActive ? 'red' : '#fff',
+  })}>Science</NavLink>
+          </li>
+          <li className="nav-item" style={navNames1}>
+          <NavLink to="/technology" className="links" style={({ isActive }) => ({
+    color: isActive ? 'red' : '#fff',
+  })}>Technology</NavLink>
           </li>
         </ul>
       </div>
